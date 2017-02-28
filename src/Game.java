c6e263e4f1751a49f5c1244dfc8faf1a988db362
Andrew Harris <blakebharris@gmail.com>
2017-02-28 13:55:33 -0600

    added the view query and add new game function

--- src/Game.java
+++ src/Game.java
@@ -2,13 +2,15 @@
  * Created by BHarris on 2/22/17.
  */
 public class Game {
+    int id;
     String name;
     String genre;
     String platform;
     int releaseYear;
 
 
-    public Game(String name, String genre, String platform, int releaseYear) {
+    public Game(int id, String name, String genre, String platform, int releaseYear) {
+        this.id = id;
         this.name = name;
         this.genre = genre;
         this.platform = platform;
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,10 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Main {
     public static void addNewGame(Connection conn, String game_name, String genre, String platform, int game_year) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("INSERT INTO todos VALUES (NULL, ?, ?, ?, ?);");
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO games VALUES (NULL, ?, ?, ?, ?);");
 
         stmt.setString(1, game_name);
         stmt.setString(2, genre);
@@ -19,23 +18,40 @@ public class Main {
         stmt.execute();
     }
 
+    public static ArrayList<Game> getGames(Connection conn) throws SQLException {
+        ArrayList<Game> games = new ArrayList<>();
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM games;");
+        ResultSet results = stmt.executeQuery();
+        while(results.next()) {
+            int id = results.getInt("id");
+            String game_name = results.getString("game_name");
+            String genre  = results.getString("genre");
+            String platform = results.getString("platform");
+            int release_year = results.getInt("release_year");
+            games.add(new Game(id, game_name, genre, platform, release_year ));
+        }
+
+        return games;
+    }
+
+    public static void updateGame(Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM games;");
+        ResultSet results = stmt.executeQuery();
+    }
+
     static ArrayList<Game> games = new ArrayList<>();
     public static void main(String[] args) throws SQLException {
         Server.createWebServer().start();
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
         Statement stmt = conn.createStatement();
-        stmt.execute("CREATE TABLE IF NOT EXISTS todos (id IDENTITY, game_name VARCHAR, genre VARCHAR, platform VARCHAR, release_year INT );");
+        stmt.execute("CREATE TABLE IF NOT EXISTS games (id IDENTITY, game_name VARCHAR, genre VARCHAR, platform VARCHAR, release_year INT );");
         System.out.println("GameTracker starting...");
 
         Spark.init();
 
         Spark.get("/", (request, response) -> {
-            Session session = request.session();
-            String name = session.attribute("userName");
 
-            HashMap m = new HashMap();
-
-            return new ModelAndView(games, "home.html");
+            return new ModelAndView(getGames(conn), "home.html");
 
         }, new MustacheTemplateEngine());
 
@@ -47,8 +63,7 @@ public class Main {
             String platform = request.queryParams("platform");
             int gameYear = Integer.parseInt(request.queryParams("gameYear"));
 
-            Game game = new Game(gameName, gameGenre, platform, gameYear);
-            games.add(game);
+            addNewGame(conn, gameName, gameGenre, platform, gameYear);
 
             response.redirect("/");
 
